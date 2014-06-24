a29d611846e3419909d22e7e2b4ec32dc319e356
Daisuke Yamashita <dddaisuke@gmail.com>
2014-06-24 06:47:01 -0700

    サンプルコードの追加

--- src/me/renews/RenewsMeServlet.java
+++ src/me/renews/RenewsMeServlet.java
@@ -1,13 +1,22 @@
 package me.renews;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.smu.tspell.wordnet.Synset;
+import edu.smu.tspell.wordnet.WordNetDatabase;
 
 @SuppressWarnings("serial")
 public class RenewsMeServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		WordNetDatabase database = WordNetDatabase.getFileInstance();
+
+		Synset[] synsets = database.getSynsets("messages");
+		resp.getWriter().println(synsets[0].getWordForms()[0]);
 	}
 }
