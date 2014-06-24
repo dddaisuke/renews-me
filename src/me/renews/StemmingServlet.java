5548da0bba63656061826fb22fbc956eabfc2a7c
Daisuke Yamashita <dddaisuke@gmail.com>
2014-06-24 13:37:26 -0700

    StemmingServletを追加

--- src/me/renews/StemmingServlet.java
+++ src/me/renews/StemmingServlet.java
@@ -9,14 +9,20 @@ import javax.servlet.http.HttpServletResponse;
 import edu.smu.tspell.wordnet.Synset;
 import edu.smu.tspell.wordnet.WordNetDatabase;
 
-@SuppressWarnings("serial")
-public class RenewsMeServlet extends HttpServlet {
+public class StemmingServlet extends HttpServlet {
+
+	private static final String WORD = "word";
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
+		String word = req.getParameter(WORD);
+		if (word == "") {
+			return;
+		}
 		WordNetDatabase database = WordNetDatabase.getFileInstance();
 
-		Synset[] synsets = database.getSynsets("messages");
+		Synset[] synsets = database.getSynsets(word);
 		resp.getWriter().println(synsets[0].getWordForms()[0]);
 	}
 }
